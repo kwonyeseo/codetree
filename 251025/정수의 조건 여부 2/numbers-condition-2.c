383910bf5dc8064f6e0577bfc43029c52f484eb8
kwonyeseo <a01045916545@gmail.com>
2025-10-25 02:31:30 +0900

    Title: 정수의 조건 여부 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/정수의 조건 여부 2/numbers-condition-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d", &a);
+
+    if(a == 5) {
+        printf("A");
+    }else if(a % 2 == 0) {
+        printf("B");
+    }
+    return 0;
+}
