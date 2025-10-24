a1b2ced65e92c8e104dda852bb41bd7cb4bd49af
kwonyeseo <a01045916545@gmail.com>
2025-10-25 02:35:21 +0900

    Title: 두 수의 짝홀 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/두 수의 짝홀 여부/parity-of-two-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a % 2 == 0) {
+        printf("even\n");
+    }else {
+        printf("odd\n");
+    }
+
+    if (b % 2 == 0) {
+        printf("even\n");
+    }else {
+        printf("odd\n");
+    }
+    return 0;
+}
