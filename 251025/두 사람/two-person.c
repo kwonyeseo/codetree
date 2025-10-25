e2f24c1f73be0e4791a1453850856c1504dcbd5e
kwonyeseo <a01045916545@gmail.com>
2025-10-25 12:46:20 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251025/두 사람/two-person.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a1, a2;
+    char s1, s2;
+    scanf("%d %c %d %d", &a1, &s1, &a2, &s2);
+
+    printf("%d", (a1 >= 19 && s1 == 'M') || (a2 >= 19 && s2 == 'M'));
+    return 0;
+}
