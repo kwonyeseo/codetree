81dd559e3da926adaf76039f78c484e2efb41e07
kwonyeseo <a01045916545@gmail.com>
2025-10-25 15:26:35 +0900

    Title: 홀수만 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/홀수만 출력/output-only-odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if (a % 2 == 0) {
+        a++;
+    }
+    for (int i = a; i <= b; i += 2) {
+        printf("%d ", i);
+    }
+    return 0;
+}
