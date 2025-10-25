cad8bf6f83fe486ae0850fd74816e3f8cf06ca0f
kwonyeseo <a01045916545@gmail.com>
2025-10-25 12:48:46 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251025/두 사람/two-person.c
+++ 251025/두 사람/two-person.c
@@ -4,7 +4,7 @@ int main() {
     // Please write your code here.
     int a1, a2;
     char s1, s2;
-    scanf("%d %c %d %d", &a1, &s1, &a2, &s2);
+    scanf("%d %c %d %c", &a1, &s1, &a2, &s2);
 
     printf("%d", (a1 >= 19 && s1 == 'M') || (a2 >= 19 && s2 == 'M'));
     return 0;
