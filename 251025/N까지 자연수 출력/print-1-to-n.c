0b3b0a8a5b423820cd7920b3cbddba8de45f6cfb
kwonyeseo <a01045916545@gmail.com>
2025-10-25 15:37:30 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/N까지 자연수 출력/print-1-to-n.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    int i = 1;
+    scanf("%d", &n);
+
+    while (i <= n) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
