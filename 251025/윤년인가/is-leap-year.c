b9e24722188f0b799d65ed113a1dc44daa54496d
kwonyeseo <a01045916545@gmail.com>
2025-10-25 13:00:43 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/윤년인가/is-leap-year.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int y;
+    scanf("%d", &y);
+
+    if (y % 4 == 0) {
+        if (y % 100 == 0) {
+            if (y % 400 == 0) {
+                printf("true");
+            }else {
+                printf("false");
+            }
+        }else {
+            printf("true");
+        }
+    }else {
+        printf("false");
+    }
+    return 0;
+}
