9a695dd8e588c426b54594095802996ac565059f
kwonyeseo <a01045916545@gmail.com>
2025-12-31 14:06:11 +0900

    Title: 완전수 판별기, Time: 111ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251231/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int perf = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                perf += i;
+            }
+        }
+        if (n == perf) {
+            System.out.println("P");
+        }else {
+            System.out.println("N");
+        }
+    }
+}
