4c85f6f3c38fe97d1f29e1f81d8e6eefbc5935f0
kwonyes! <a01045916545@gmail.com>
2026-03-09 10:31:57 +0900

    Title: 조건에 따른 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/조건에 따른 연산/operatino-by-rule.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int cnt = 0;
+    scanf("%d", &n);
+    while (n <= 1000) {
+        if (n % 2 == 0) {
+            n = n * 3 + 1;
+            cnt++;
+        }else {
+            n = n * 2 + 2;
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
