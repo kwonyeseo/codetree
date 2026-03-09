0e7027dccab5c55d69685c4050d8c0f28e8224d0
kwonyes! <a01045916545@gmail.com>
2026-03-09 11:30:23 +0900

    Title: 별표 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260309/별표 출력하기 2/print-star-2.c
+++ 260309/별표 출력하기 2/print-star-2.c
@@ -3,13 +3,11 @@
 int main() {
     int n;
     scanf("%d", &n);
-    int c = n;
 
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < c; i++) {
+    for (int k = n; k > 0; k--) {
+        for (int i = 0; i < k; i++) {
             printf("* ");
         }
-        c--;
         printf("\n");
     }
     return 0;
