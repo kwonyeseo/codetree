7b721d37a8711e49832c2fdde3d246265532d6d2
kwonyes! <a01045916545@gmail.com>
2026-03-09 11:04:35 +0900

    Title: 소수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/소수 판별기/prime-number-discriminator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    bool satisfied = false;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            satisfied = true;
+        }
+    }
+    if (satisfied == true) {
+        printf("C");
+    }else{
+        printf("P");
+    }
+    return 0;
+}
