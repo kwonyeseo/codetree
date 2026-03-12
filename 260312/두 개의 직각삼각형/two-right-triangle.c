b14f1817c2af09990b8c66e5b825314f84ab3a0a
kwonyes! <a01045916545@gmail.com>
2026-03-12 11:23:58 +0900

    Title: 두 개의 직각삼각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260312/두 개의 직각삼각형/two-right-triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n - i; k++) {
+            printf("*");
+        }for (int j = 0; j < i * 2; j++) {
+            printf(" ");
+        }for (int k = 0; k < n - i; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
