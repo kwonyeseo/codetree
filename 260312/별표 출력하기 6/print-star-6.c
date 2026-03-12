fda730d495138694066c667748ae05b3f786f280
kwonyes! <a01045916545@gmail.com>
2026-03-12 13:54:02 +0900

    Title: 별표 출력하기 6, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260312/별표 출력하기 6/print-star-6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < i; k++) {
+            printf("  ");
+        }
+        for (int j = 0; j < (n - i) * 2 - 1; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int k = 1; k < n - i; k++) {
+            printf("  ");
+        }
+        for (int j = 0; j < i * 2 + 1; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
